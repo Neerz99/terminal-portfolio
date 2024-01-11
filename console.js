20cf4afac582713feafba5a7f822cf873cbd2086
iloenzo99@gmail.com <iloenzo99@gmail.com>
2024-01-12 00:11:48 +0100

    added command history browsing

--- console.js
+++ console.js
@@ -1,32 +1,38 @@
 let terminalInput = document.getElementById("terminalInput");
-terminalInput.addEventListener("keydown", function (event) {
-    if (event.key === 'Enter') { // Enter key
-        event.preventDefault(); // Prevent form submission
-        history()
-        document.getElementById("submitHidden").click(); // Trigger the hidden submit button
-        helpCommand()
-        aboutCommand()
-        linksCommand()
-        clearInput()
-    }
-});
-
 let terminalOutput = document.getElementById("terminalOutput");
-
 let commandHistory = [];
+let currentIndex = 0;
 
-// only goes back to the latest command for now !!!
 terminalInput.addEventListener("keydown", function (event) {
-    if (event.key === 'ArrowUp') {
+    if (event.key === 'Enter') {
         event.preventDefault();
-        terminalInput.value = commandHistory[commandHistory.length - 1]
-
+        history();
+        document.getElementById("submitHidden").click();
+        helpCommand();
+        aboutCommand();
+        linksCommand();
+        clearInput();
+    } else if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        if (currentIndex > 0) {
+            currentIndex--;
+            terminalInput.value = commandHistory[currentIndex];
+        }
+    } else if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        if (currentIndex < commandHistory.length - 1) {
+            currentIndex++;
+            terminalInput.value = commandHistory[currentIndex];
+        } else {
+            currentIndex = commandHistory.length;
+            terminalInput.value = "";
+        }
     }
 });
 
 function history() {
-    commandHistory.push(terminalInput.value)
-    console.log(commandHistory)
+    commandHistory.push(terminalInput.value);
+    currentIndex = commandHistory.length;
 }
 
 let helpList = ['help', 'clear', 'about', 'contact', 'projects', 'skills', 'education', 'experience', 'resume', 'github', 'linkedin', 'email', 'phone'];
@@ -39,18 +45,18 @@ function clearInput() {
 // Because of column-reverse, the first element is the last child
 function helpCommand() {
     if (terminalInput.value == 'help') {
-        terminalOutput.innerHTML += "<br><p>Available commands: " + helpList + "</p><br><p>help</p>"
+        terminalOutput.innerHTML += "<br><p>Available commands: " + helpList + "</p><br><p>Help</p>"
     }
 }
 
 function aboutCommand() {
     if (terminalInput.value == 'about') {
-        terminalOutput.innerHTML += "<br><p>I am Enzo ILO, passionate web developer from France. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore unde, laudantium deleniti beatae et cupiditate nihil voluptatum exercitationem rem rerum quis, esse aspernatur incidunt. Commodi repellendus nihil quam sapiente labore.</p><br><p>about</p>"
+        terminalOutput.innerHTML += "<br><p>I am Enzo ILO, passionate web developer from France. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore unde, laudantium deleniti beatae et cupiditate nihil voluptatum exercitationem rem rerum quis, esse aspernatur incidunt. Commodi repellendus nihil quam sapiente labore.</p><br><p>About</p>"
     }
 }
 
 function linksCommand() {
     if (terminalInput.value == 'links') {
-        terminalOutput.innerHTML += "<br><p>Github: <a href='https://www.github.com/Neerz99' target='_blank'><u>github.com/Neerz99</u></a></p><p>Linkedin: <a href='linkedin.com'><u>linkedin.com</u></a></p><br><p>links</p>"
+        terminalOutput.innerHTML += "<br><p>Github: <a href='https://www.github.com/Neerz99' target='_blank'><u>github.com/Neerz99</u></a></p><p>Linkedin: <a href='linkedin.com'><u>linkedin.com</u></a></p><br><p>Links</p>"
     }
 }
