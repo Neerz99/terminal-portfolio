c82116a10a462afcf2ea86d4c0664aac54b4bb65
iloenzo99@gmail.com <iloenzo99@gmail.com>
2024-01-12 18:38:27 +0100

    added ASCII art

--- console.js
+++ console.js
@@ -2,7 +2,7 @@ let terminalInput = document.getElementById("terminalInput");
 let terminalOutput = document.getElementById("terminalOutput");
 let commandHistory = [];
 let currentIndex = 0;
-let helpList = ['help', 'clear', 'about', 'contact', 'projects', 'skills', 'education', 'experience', 'resume', 'github', 'linkedin', 'email', 'phone'];
+let helpList = ['help', 'clear', 'about', 'contact', 'projects', 'skills', 'education', 'experience', 'resume', 'github', 'linkedin', 'email', 'phone', 'links'];
 
 
 terminalInput.addEventListener("keydown", function (event) {
@@ -50,7 +50,7 @@ function clearInput() {
 // Returns a error message if the command is not recognized
 function unknownCommand() {
     if (!helpList.includes(terminalInput.value)) {
-        terminalOutput.innerHTML += "<br><p>Unknown command, type 'help' to see available commands</p>"
+        terminalOutput.innerHTML += "<br><br><p>Unknown command, type 'help' to see available commands.</p>"
     }
 }
 
@@ -60,7 +60,7 @@ function unknownCommand() {
 
 function helpCommand() {
     if (terminalInput.value == 'help') {
-        terminalOutput.innerHTML += "<br><p>Available commands: " + helpList + "</p><br><p>Help</p>"
+        terminalOutput.innerHTML += "<br><br><p>Available commands: " + helpList + "</p><br><p>Help:</p>"
     }
 }
 
@@ -72,13 +72,13 @@ function clearCommand() {
 
 function aboutCommand() {
     if (terminalInput.value == 'about') {
-        terminalOutput.innerHTML += "<br><p>I am Enzo ILO, passionate web developer from France. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore unde, laudantium deleniti beatae et cupiditate nihil voluptatum exercitationem rem rerum quis, esse aspernatur incidunt. Commodi repellendus nihil quam sapiente labore.</p><br><p>About</p>"
+        terminalOutput.innerHTML += "<br><p>I am Enzo ILO, passionate web developer from France. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore unde, laudantium deleniti beatae et cupiditate nihil voluptatum exercitationem rem rerum quis, esse aspernatur incidunt. Commodi repellendus nihil quam sapiente labore.</p><br><pre>########## ###    #### ######### ########          ########### ########## ######## </pre><pre> #+#        #+#   #+#+#  #+#     #+#    #+#             #+#     #+#       #+#    #+#     </pre><pre>  +#+        +#+  +#+#+#   +#+    +#+    +#+             +#+     +#+       +#+    +#+    </pre><pre></pre   +#++:++#   +#+ +:+ +#+    +#+   +#+    +:+             +#+     +#+       +#+    +:+   ><pre>    +:+:+      :+:+:+  +:+     +:+  +:+    +:+             +:+     +:+       +:+    +:+  </pre><pre>     :+:        :+:+:   :+:      :+: :+:    :+:             :+:     :+:       :+:    :+: </pre><pre>      :::::::::: ::::    ::: ::::::::: ::::::::          ::::::::::: :::        :::::::: </pre><br><p>About:</p>"
     }
 }
 
 function linksCommand() {
     if (terminalInput.value == 'links') {
-        terminalOutput.innerHTML += "<br><p>Github: <a href='https://www.github.com/Neerz99' target='_blank'><u>github.com/Neerz99</u></a></p><p>Linkedin: <a href='linkedin.com'><u>linkedin.com</u></a></p><br><p>Links</p>"
+        terminalOutput.innerHTML += "<br><br><p>Github: <a href='https://www.github.com/Neerz99' target='_blank'><u>github.com/Neerz99</u></a></p><p>Linkedin: <a href='linkedin.com'><u>linkedin.com</u></a></p><br><p>Links:</p>"
     }
 }
 
