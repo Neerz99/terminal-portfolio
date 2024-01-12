ac0045d181c1683c941cc8e4583c390e82a4633a
iloenzo99@gmail.com <iloenzo99@gmail.com>
2024-01-12 16:29:03 +0100

    added commands

--- console.js
+++ console.js
@@ -2,6 +2,8 @@ let terminalInput = document.getElementById("terminalInput");
 let terminalOutput = document.getElementById("terminalOutput");
 let commandHistory = [];
 let currentIndex = 0;
+let helpList = ['help', 'clear', 'about', 'contact', 'projects', 'skills', 'education', 'experience', 'resume', 'github', 'linkedin', 'email', 'phone'];
+
 
 terminalInput.addEventListener("keydown", function (event) {
     if (event.key === 'Enter') {
@@ -11,6 +13,8 @@ terminalInput.addEventListener("keydown", function (event) {
         helpCommand();
         aboutCommand();
         linksCommand();
+        unknownCommand();
+        clearCommand();
         clearInput();
     } else if (event.key === 'ArrowUp') {
         event.preventDefault();
@@ -30,22 +34,39 @@ terminalInput.addEventListener("keydown", function (event) {
     }
 });
 
+// Adds the command to the command history
 function history() {
     commandHistory.push(terminalInput.value);
     currentIndex = commandHistory.length;
 }
 
-let helpList = ['help', 'clear', 'about', 'contact', 'projects', 'skills', 'education', 'experience', 'resume', 'github', 'linkedin', 'email', 'phone'];
 
+
+// Clears the input field after submitting
 function clearInput() {
     terminalInput.value = "";
 }
 
+// Returns a error message if the command is not recognized
+function unknownCommand() {
+    if (!helpList.includes(terminalInput.value)) {
+        terminalOutput.innerHTML += "<br><p>Unknown command, type 'help' to see available commands</p>"
+    }
+}
+
+
 // Commands
 // Because of column-reverse, the first element is the last child
+
 function helpCommand() {
     if (terminalInput.value == 'help') {
         terminalOutput.innerHTML += "<br><p>Available commands: " + helpList + "</p><br><p>Help</p>"
+    }
+}
+
+function clearCommand() {
+    if (terminalInput.value == 'clear') {
+        terminalOutput.innerHTML = "";
     }
 }
 
@@ -60,3 +81,4 @@ function linksCommand() {
         terminalOutput.innerHTML += "<br><p>Github: <a href='https://www.github.com/Neerz99' target='_blank'><u>github.com/Neerz99</u></a></p><p>Linkedin: <a href='linkedin.com'><u>linkedin.com</u></a></p><br><p>Links</p>"
     }
 }
+
